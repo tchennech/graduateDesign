945c239a02c3f263f916bbe634fe54e8ee9b751f
tchennech <1729796645@qq.com>
2019-03-28 16:47:02 +0800

    加入读取模型

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import os
-from preperData import * 
+from preperData import *
 from ResNextNet import ResNeXt
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -31,6 +31,8 @@ x = None
 label = None
 learning_rate = None
 training_flag = None
+
+
 def Evaluate(sess):
     test_acc = 0.0
     test_loss = 0.0
@@ -63,113 +65,123 @@ def Evaluate(sess):
     return test_acc, test_loss, summary
 
 
-def trainModel(savePath):  
-  global epoch_learning_rate, cost, accuracy, train_x, train_y, test_x, test_y, x, label, learning_rate, training_flag
-  #数据集路径
-  path = "./data/Category-"+"32"
-  totalImage, totalFlag = readImage(path)
+def trainModel(savePath):
+    global epoch_learning_rate, cost, accuracy, train_x, train_y, test_x, test_y, x, label, learning_rate, training_flag
+    # 数据集路径
+    path = "./data/Category-"+"32"
+    totalImage, totalFlag = readImage(path)
 
-  #裁剪长度，为了方便，测试集为2000
-  length = 7160
-  train_x, train_y = totalImage[:length], totalFlag[:length]
-  test_x, test_y = totalImage[length:], totalFlag[:length:]
-  image_size = 32
-  img_channels = 3
-  class_num = 1 #实际8
-  x = tf.placeholder(tf.float32, shape=[
-                  None, image_size, image_size, img_channels])
-  label = tf.placeholder(tf.float32, shape=[None, class_num])
+    # 裁剪长度，为了方便，测试集为2000
+    length = 7160
+    train_x, train_y = totalImage[:length], totalFlag[:length]
+    test_x, test_y = totalImage[length:], totalFlag[:length:]
+    image_size = 32
+    img_channels = 3
+    class_num = 1  # 实际8
+    x = tf.placeholder(tf.float32, shape=[
+        None, image_size, image_size, img_channels])
+    label = tf.placeholder(tf.float32, shape=[None, class_num])
 
-  training_flag = tf.placeholder(tf.bool)
-  logits = ResNeXt(x, training=training_flag).model
-  learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-  epoch_learning_rate = init_learning_rate
-  cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+    training_flag = tf.placeholder(tf.bool)
+    logits = ResNeXt(x, training=training_flag).model
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    epoch_learning_rate = init_learning_rate
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
         logits=logits, labels=label))
-  l2_loss = tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
+    l2_loss = tf.add_n([tf.nn.l2_loss(var)
+                        for var in tf.trainable_variables()])
 
-  #正则化
-  optimizer = tf.train.MomentumOptimizer(
-      learning_rate=learning_rate, momentum=momentum, use_nesterov=True)
-  train = optimizer.minimize(cost + l2_loss * weight_decay)
+    # 正则化
+    optimizer = tf.train.MomentumOptimizer(
+        learning_rate=learning_rate, momentum=momentum, use_nesterov=True)
+    train = optimizer.minimize(cost + l2_loss * weight_decay)
 
-  correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(label, 1))
+    # 准确率
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(label, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    saver = tf.train.Saver(tf.global_variables())
+    tf.add_to_collection("predict", logits)
+    with tf.Session() as sess:
+        ckpt = tf.train.get_checkpoint_state('./model')
+        if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        else:
+            sess.run(tf.global_variables_initializer())
+
+        summary_writer = tf.summary.FileWriter('./logs', sess.graph)
+
+        for epoch in range(1, total_epochs + 1):
+            if epoch == (total_epochs * 0.5) or epoch == (total_epochs * 0.75):
+                epoch_learning_rate = epoch_learning_rate / 10
+
+            pre_index = 0
+            train_acc = 0.0
+            train_loss = 0.0
+
+            for step in range(1, iteration):
+                if pre_index + batch_size < enoughSize:
+                    batch_x = train_x[pre_index: pre_index + batch_size]
+                    batch_y = train_y[pre_index: pre_index + batch_size]
+                else:
+                    batch_x = train_x[pre_index:]
+                    batch_y = train_y[pre_index:]
+                #print(pre_index, batch_size)
+                batch_x = data_augmentation(batch_x)
+                train_feed_dict = {
+                    x: batch_x,
+                    label: batch_y,
+                    learning_rate: epoch_learning_rate,
+                    training_flag: True
+                }
+
+                _, batch_loss = sess.run(
+                    [train, cost], feed_dict=train_feed_dict)
+                batch_acc = accuracy.eval(feed_dict=train_feed_dict)
+
+                train_loss += batch_loss
+                train_acc += batch_acc
+                pre_index += batch_size
+
+            train_loss /= iteration  # average loss
+            train_acc /= iteration  # average accuracy
+
+            train_summary = tf.Summary(value=[tf.Summary.Value(tag='train_loss', simple_value=train_loss),
+                                              tf.Summary.Value(tag='train_accuracy', simple_value=train_acc)])
+
+            test_acc, test_loss, test_summary = Evaluate(sess)
+
+            summary_writer.add_summary(
+                summary=train_summary, global_step=epoch)
+            summary_writer.add_summary(summary=test_summary, global_step=epoch)
+            summary_writer.flush()
+
+            line = "epoch: %d/%d, train_loss: %.4f, train_acc: %.4f, test_loss: %.4f, test_acc: %.4f \n" % (
+                epoch, total_epochs, train_loss, train_acc, test_loss, test_acc)
+            print(line)
+
+            with open('logs.txt', 'a') as f:
+                f.write(line)
+
+            saver.save(sess=sess, save_path='./model/'+savePath+'.ckpt')
 
 
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
-  saver = tf.train.Saver(tf.global_variables())
-  with tf.Session() as sess:
-      ckpt = tf.train.get_checkpoint_state('./model')
-      if ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path):
-          saver.restore(sess, ckpt.model_checkpoint_path)
-      else:
-          sess.run(tf.global_variables_initializer())
-
-      summary_writer = tf.summary.FileWriter('./logs', sess.graph)
-
-      
-      for epoch in range(1, total_epochs + 1):
-          if epoch == (total_epochs * 0.5) or epoch == (total_epochs * 0.75):
-              epoch_learning_rate = epoch_learning_rate / 10
-
-          pre_index = 0
-          train_acc = 0.0
-          train_loss = 0.0
-
-          for step in range(1, iteration):
-              if pre_index + batch_size < enoughSize:
-                  batch_x = train_x[pre_index: pre_index + batch_size]
-                  batch_y = train_y[pre_index: pre_index + batch_size]
-              else:
-                  batch_x = train_x[pre_index:]
-                  batch_y = train_y[pre_index:]
-              #print(pre_index, batch_size)
-              batch_x = data_augmentation(batch_x)
-              train_feed_dict = {
-                  x: batch_x,
-                  label: batch_y,
-                  learning_rate: epoch_learning_rate,
-                  training_flag: True
-              }
-
-              _, batch_loss = sess.run([train, cost], feed_dict=train_feed_dict)
-              batch_acc = accuracy.eval(feed_dict=train_feed_dict)
-
-              train_loss += batch_loss
-              train_acc += batch_acc
-              pre_index += batch_size
-
-          train_loss /= iteration  # average loss
-          train_acc /= iteration  # average accuracy
-
-          train_summary = tf.Summary(value=[tf.Summary.Value(tag='train_loss', simple_value=train_loss),
-                                            tf.Summary.Value(tag='train_accuracy', simple_value=train_acc)])
-
-          test_acc, test_loss, test_summary = Evaluate(sess)
-
-          summary_writer.add_summary(summary=train_summary, global_step=epoch)
-          summary_writer.add_summary(summary=test_summary, global_step=epoch)
-          summary_writer.flush()
-
-          line = "epoch: %d/%d, train_loss: %.4f, train_acc: %.4f, test_loss: %.4f, test_acc: %.4f \n" % (
-              epoch, total_epochs, train_loss, train_acc, test_loss, test_acc)
-          print(line)
-
-          with open('logs.txt', 'a') as f:
-              f.write(line)
-
-          saver.save(sess=sess, save_path='./model/'+savePath+'.ckpt')
 def readModel(name):
-  aaa = 1
+    datasName = input("输入数据集名字")
+    X = preperDatas(datasName)
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph('./model/'+name)
+        saver.restore(sess, tf.train.latest_checkpoint("./model/"))
+        result = tf.get_collection("predict")
+        pred = sess.run(result, feed_dict={x: X})
+        print(pred)
+
+
 if __name__ == '__main__':
     function = int(input("选择功能，0：训练；1：分类"))
     if(function):
-      modelName = input("输入模型名字")
-      readModel(modelName)
+        modelName = input("输入模型名字")
+        readModel(modelName)
     else:
-      modelName = input("输入模型名字")
-      trainModel(modelName)
-    
-    
-    
+        modelName = input("输入模型名字")
+        trainModel(modelName)
--- preperData.py
+++ preperData.py
@@ -12,10 +12,12 @@ image_size = 32
 img_channels = 3
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-#读取数据集 总共8类 32, 64, 128 , 256尺寸
-#os opencv
+# 读取数据集 总共8类 32, 64, 128 , 256尺寸
+# os opencv 训练数据集
+
+
 def readImage(dir):
-    totalImage =  []
+    totalImage = []
     totalFlag = []
     for i in range(8):
         dirName = "10"+str(i+1)
@@ -28,20 +30,36 @@ def readImage(dir):
                 continue
             totalImage.append(img)
             totalFlag.append([i])
-    #转成矩阵
-    totalImage =  np.array(totalImage)
+    # 转成矩阵
+    totalImage = np.array(totalImage)
     totalFlag = np.array(totalFlag)
     return totalImage, totalFlag
 
-#随机裁剪
+
+# 实际用到的数据集
+def preperDatas(dir):
+    totalImage = []
+    realPath = './data/' + dir
+    for fileName in os.listdir(realPath):
+        img = cv2.imread(realPath+"/"+fileName)
+        if img is None:
+            continue
+        totalImage.append(img)
+    # 转成矩阵
+    totalImage = np.array(totalImage)
+    return totalImage
+
+# 随机裁剪
+
+
 def _random_crop(batch, crop_shape, padding=None):
-    #保存初始形状
+    # 保存初始形状
     oshape = np.shape(batch[0])
 
     if padding:
         oshape = (oshape[0] + 2 * padding, oshape[1] + 2 * padding)
     new_batch = []
-    #填充形状 上下左右都是 padding
+    # 填充形状 上下左右都是 padding
     npad = ((padding, padding), (padding, padding), (0, 0))
     for i in range(len(batch)):
         new_batch.append(batch[i])
@@ -51,18 +69,22 @@ def _random_crop(batch, crop_shape, padding=None):
         nh = random.randint(0, oshape[0] - crop_shape[0])
         nw = random.randint(0, oshape[1] - crop_shape[1])
         new_batch[i] = new_batch[i][nh:nh + crop_shape[0],
-                       nw:nw + crop_shape[1]]
+                                    nw:nw + crop_shape[1]]
     return new_batch
 
-#随机翻转
+# 随机翻转
+
+
 def _random_flip_leftright(batch):
     for i in range(len(batch)):
-        #随机1/2概率
+        # 随机1/2概率
         if bool(random.getrandbits(1)):
             batch[i] = np.fliplr(batch[i])
     return batch
 
-#处理数据
+# 处理数据
+
+
 def data_augmentation(batch):
     batch = _random_flip_leftright(batch)
     #修剪为[32, 32]
